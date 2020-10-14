94a841f321af47cdda061ab567e3199294dee6eb
dhritidesai <68687733+dhritidesai@users.noreply.github.com>
2020-10-14 15:57:54 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,75 @@
+var p1= document.getElementById("page1");
+var p2= document.getElementById("page2");
+
+var startingX;
+
+function p1Start(evt){
+  startingX= evt.touches[0].clientX;
+  p1.style.transition= '';
+  p2.style.transition= '';
+}
+
+function p1Move(evt){
+  var touch= evt.touches[0];
+  var change= startingX - touch.clientX;
+  if (change<0) {
+    return;
+  }
+  p1.style.left= '-' + change + 'px';
+  p2.style.display= 'block';
+  p2.style.left= (screen.width - change) + 'px';
+  evt.preventDefault();
+}
+
+function p1End(evt){
+  var change= startingX - evt.changedTouches[0].clientX;
+  var diff= screen.width / 3;
+  if (change<diff) {
+    p1.style.left= 0;
+    p2.style.left= '100%';
+    p2.style.display= 'none';
+  }
+  else{
+    p1.style.transition= 'all .3s';
+    p2.style.transition= 'all .3s';
+    p1.style.left= '-100%';
+    p2.style.left= '0';
+    p2.style.display= 'block';
+  }
+}
+
+function p2Start(evt){
+  startingX= evt.touches[0].clientX;
+  p1.style.transition= '';
+  p2.style.transition= '';
+  p1.style.display= 'none';
+}
+
+function p2Move(evt){
+  var touch= evt.touches[0];
+  var change=  touch.clientX - startingX;
+  if (change<0) {
+    return;
+  }
+  p1.style.display= 'block';
+  p1.style.left= (change - screen.width) + 'px';
+  p2.style.left= change + 'px';
+  evt.preventDefault();
+}
+
+function p2End(evt){
+  var change= evt.changedTouches[0].clientX - startingX;
+  var diff= screen.width / 4;
+  if (change<diff) {
+    p1.style.left= '-100%';
+    p1.style.display= 'none';
+    p2.style.left= 0;
+  }
+  else{
+    p1.style.transition= 'all .3s';
+    p2.style.transition= 'all .3s';
+    p1.style.left= 0;
+    p2.style.left= '100%';
+    p2.style.display='none';
+  }
+}
